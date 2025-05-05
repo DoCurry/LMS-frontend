2c1f329019e83814cb8f636a304edad995dd7a19
DoCurry <152831595+DoCurry@users.noreply.github.com>
2025-05-05 16:55:35 +0545

    fix: removed flowbite and added shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 
 import { createRoot } from 'react-dom/client'
 import "./assets/global.css";
-import "flowbite";
 import RoutingConfig from './config/routing.config.tsx'
 
 createRoot(document.getElementById('root')!).render(
