9d1c83edc3aad155c2a2b65f94b8e17c35242ddb
DoCurry <152831595+DoCurry@users.noreply.github.com>
2025-05-05 17:08:14 +0545

    fix: removed flowbite and added shadcn continued

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import "./assets/global.css";
 import RoutingConfig from './config/routing.config.tsx'
+import { ThemeProvider } from './components/ui/theme-provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <RoutingConfig />
+  <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+    <RoutingConfig />
+  </ThemeProvider>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,9 @@
 export default {
   content: [
     "./src/**/*.{ts,tsx,js,jsx}",
-    "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
